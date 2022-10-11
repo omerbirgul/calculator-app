225361d2393be6ba5c4d1907f90881585dc3cba6
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2022-10-11 14:12:46 +0300

    Add_operation_functions

--- script.js
+++ script.js
@@ -22,16 +22,53 @@ class Calculator {
     }
 
     chooseOperation(operation){
+        if(this.currentOperand === " ") return
+        if(this.previousOperand !== " "){
+            this.compute() 
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = " ";
 
     }
 
     compute(){
+        let computation;
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if(isNaN(prev) || isNaN(current)) return
+
+        switch(this.operation){
+            case '+':
+                computation = prev + current;
+                break;
+
+            case "-":
+                computation = prev - current;
+                break;
+
+            case "÷":
+                computation = prev / current;
+                break;
+
+            case "*":
+                computation = prev * current;
+                break;
+
+            default:
+                return
+        }
+
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = " ";
 
     }
 
 
     updateDisplay(){
-        this.currentOperandTextElement.innerText = this.currentOperand
+        this.currentOperandTextElement.innerText = this.currentOperand;
+        this.previousOperandTextElement.innerText = this.previousOperand;
 
     }
 
@@ -56,4 +93,17 @@ numberButtons.forEach(button => {
     calculator.appendNumber(button.innerText)
     calculator.updateDisplay()
   })
+})
+
+
+operationButtons.forEach(button =>{
+    button.addEventListener("click", () =>{
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+equalsButtons.addEventListener("click", () =>{
+    calculator.compute();
+    calculator.updateDisplay();
 })
